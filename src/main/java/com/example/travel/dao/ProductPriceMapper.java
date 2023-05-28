7483a0c54b4d33e8c51f2e1d41af72b68c2ade32
yijiyinm <yijiyin@outlook.com>
2023-05-28 20:04:09 +0800

    查询字段

--- src/main/java/com/example/travel/dao/ProductPriceMapper.java
+++ src/main/java/com/example/travel/dao/ProductPriceMapper.java
@@ -13,7 +13,7 @@ import java.util.List;
  */
 public interface ProductPriceMapper extends BaseMapper<ProductPriceDO> {
 
-    @Select("select price,price_cr,price_et,day_date from product_price where product_code=#{productCode} and day_date >= #{day} order by day_date ")
+    @Select("select price,price_cr,price_et,day_date,inventory from product_price where product_code=#{productCode} and day_date >= #{day} order by day_date ")
     List<ProductPriceDO> getPriceInfoByDay(@Param("productCode") String productCode, @Param("day") String day);
 
     @Delete("delete from product_price where product_code=#{productCode} and day_date >= #{day} order by day_date ")
